c3ca69b861d7a66b1f7a9ea09d7871f5d85a723c
Soniya-Thapa <ayinos.apaht143@gmail.com>
2025-10-04 18:55:06 +0545

    teacher created by login

--- src/app.ts
+++ src/app.ts
@@ -2,19 +2,29 @@ import express from "express"
 const app = express()
 
 import authRoutes from "./route/globals/auth/auth.route"
+
+//institutes routes
 import instituteRoutes from "./route/institute/institute.route"
-import courseRoutes from "./route/institute/course/course.route"
-import studentRoutes from "./route/institute/student/student.route"
-import teacherRoutes from "./route/institute/teacher/teacher.route"
-import categoryRoutes from "./route/institute/category/category.route"
+import courseInstituteRoutes from "./route/institute/course/course.route"
+import studentInstituteRoutes from "./route/institute/student/student.route"
+import teacherInstituteRoutes from "./route/institute/teacher/teacher.route"
+import categoryInstituteRoutes from "./route/institute/category/category.route"
+
+//teacher routes
+import teacherRoutes from "../src/route/teacher/teacher.route"
 
 app.use(express.json()) //done for parsing data
 
 app.use("/auth",authRoutes)
+
+//institutes routes
 app.use("/institute",instituteRoutes)
-app.use("/institute/course",courseRoutes)
-app.use("/institute/student",studentRoutes)
-app.use("/institute/teacher",teacherRoutes)
-app.use("/institute/category",categoryRoutes)
+app.use("/institute/course",courseInstituteRoutes)
+app.use("/institute/student",studentInstituteRoutes)
+app.use("/institute/teacher",teacherInstituteRoutes)
+app.use("/institute/category",categoryInstituteRoutes)
+
+//teacher routes
+app.use("/teacher",teacherRoutes)
 
 export default app
